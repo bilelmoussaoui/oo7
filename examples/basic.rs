1249a9d8098060118d8304b7265a789523325cbe
Bilal Elmoussaoui <belmouss@redhat.com>
2022-04-23 22:19:31 +0200

    Move examples to a separate directory

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,7 +6,7 @@ use oo7::dbus::Service;
 async fn main() -> oo7::Result<()> {
     let cnx = zbus::ConnectionBuilder::session()?.build().await?;
     let service = Service::new(&cnx).await?;
-    let (out, session) = service.open_session(oo7::Algorithm::Plain).await?;
+    let (_, session) = service.open_session(oo7::Algorithm::Plain).await?;
 
     let mut attributes = HashMap::new();
     attributes.insert("type", "token");
@@ -21,17 +21,5 @@ async fn main() -> oo7::Result<()> {
             println!("{:#?}", item.secret(&session).await?);
         }
     }
-    /*let collections = service.collections().await?;
-
-    for collection in collections {
-        println!("{}", collection.label().await?);
-        println!("{}", collection.is_locked().await?);
-        println!("{:#?}", collection.created().await?);
-        println!("{:#?}", collection.modified().await?);
-
-        let items = collection.search_items(attributes).await?;
-    }
-    */
-
     Ok(())
 }
