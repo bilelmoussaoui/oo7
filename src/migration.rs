3a03a793e94274533da8f53ed6751861bf457278
Bilal Elmoussaoui <belmouss@redhat.com>
2022-04-27 01:54:43 +0200

    migration: allow passing multiple attributes

--- src/migration.rs
+++ src/migration.rs
@@ -4,16 +4,20 @@ use crate::{dbus::Service, portal::Keyring, Result};
 
 /// Helper to migrate your secrets from the host Secret Service
 /// to the sandboxed file backend.
-pub async fn migrate(attributes: HashMap<&str, &str>, replace: bool) -> Result<()> {
+pub async fn migrate(attributes: Vec<HashMap<&str, &str>>, replace: bool) -> Result<()> {
     let service = Service::new(crate::dbus::Algorithm::Encrypted).await?;
     let file_backend = Keyring::load_default().await?;
 
     let collection = service.default_collection().await?;
-    let items = collection.search_items(attributes).await?;
+    let mut all_items = Vec::default();
 
-    let mut new_items = Vec::with_capacity(items.capacity());
+    for attrs in attributes {
+        let items = collection.search_items(attrs).await?;
+        all_items.extend(items);
+    }
+    let mut new_items = Vec::with_capacity(all_items.capacity());
 
-    for item in items.iter() {
+    for item in all_items.iter() {
         let attributes = item.attributes().await?;
         let label = item.label().await?;
         let secret = item.secret().await?;
