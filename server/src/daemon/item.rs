dbdfa2beb0f9cff377e8978c723a87189308f753
Dhanuka Warusadura <dhanuka@gnome.org>
2023-12-11 11:45:41 +0530

    wip: secret-server backend implementation
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>
    Co-Author: Bilal Elmoussaoui <belmouss@redhat.com>

---
+++ server/src/daemon/item.rs
@@ -0,0 +1,122 @@
+// org.freedesktop.Secret.Item
+
+use std::{collections::HashMap, sync::Arc};
+
+use oo7::{
+    dbus::api::SecretInner,
+    portal::{self, api::AttributeValue, Keyring},
+};
+use tokio::sync::RwLock;
+use zbus::{
+    object_server::SignalContext,
+    zvariant::{ObjectPath, OwnedObjectPath},
+    ObjectServer,
+};
+
+use super::{collection::Collection, error::ServiceError, prompt::Prompt, Result};
+
+#[derive(Debug)]
+pub struct Item {
+    inner: RwLock<portal::Item>,
+    path: OwnedObjectPath,
+    keyring: Arc<Keyring>,
+    locked: bool,
+}
+
+#[zbus::interface(name = "org.freedesktop.Secret.Item")]
+impl Item {
+    pub async fn delete(
+        &self,
+        #[zbus(object_server)] object_server: &ObjectServer,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+    ) -> Result<Prompt> {
+        let inner = self.inner.read().await;
+        let attributes = inner.attributes();
+        self.keyring
+            .delete(attributes)
+            .await
+            .map_err::<ServiceError, _>(From::from)?;
+        object_server.remove::<Item, _>(self.path()).await?;
+        Collection::item_deleted(&ctxt, self.path()).await?;
+        Ok(Prompt::default())
+    }
+
+    #[zbus(name = "GetSecret")]
+    pub async fn secret(&self, session: ObjectPath<'_>) -> Result<SecretInner> {
+        let inner = self.inner.read().await;
+        let secret = inner.secret();
+        // TODO: get the session based on it object path and figure out the parameters
+        // to pass, also, what we should do about the content-type?
+        // Ok(SecretInner(session, parameters, secret.to_vec(), content_type))
+        todo!()
+    }
+
+    pub async fn set_secret(&self, secret: Vec<u8>) {
+        let mut inner = self.inner.write().await;
+        inner.set_secret(secret);
+    }
+
+    pub fn locked(&self) -> bool {
+        self.locked
+    }
+
+    pub async fn attributes(&self) -> HashMap<String, AttributeValue> {
+        let inner = self.inner.read().await;
+        inner.attributes().clone()
+    }
+
+    pub async fn set_attributes(
+        &self,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        attributes: HashMap<&str, &str>,
+    ) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.set_attributes(&attributes);
+        Collection::item_changed(&ctxt, self.path()).await?;
+        Ok(())
+    }
+
+    pub async fn label(&self) -> String {
+        let inner = self.inner.read().await;
+        inner.label().to_owned()
+    }
+
+    pub async fn set_label(
+        &self,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        label: &str,
+    ) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.set_label(label);
+        Collection::item_changed(&ctxt, self.path()).await?;
+        Ok(())
+    }
+
+    pub async fn created(&self) -> u64 {
+        self.inner.read().await.created().as_secs()
+    }
+
+    pub async fn modified(&self) -> u64 {
+        self.inner.read().await.modified().as_secs()
+    }
+}
+
+impl Item {
+    pub async fn new(
+        item: portal::Item,
+        collection_path: ObjectPath<'_>,
+        keyring: Arc<Keyring>,
+    ) -> Self {
+        Self {
+            path: OwnedObjectPath::try_from(format!("{}/items/{}", collection_path, item.label(),))
+                .unwrap(),
+            inner: RwLock::new(item),
+            keyring,
+            locked: true,
+        }
+    }
+
+    pub(crate) fn path(&self) -> ObjectPath {
+        self.path.as_ref()
+    }
+}
---
+++ server/src/daemon/service.rs
@@ -0,0 +1,256 @@
+//  org.freedesktop.Secret.Service
+
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use oo7::{
+    dbus::{
+        api::{Properties, SecretInner},
+        Algorithm,
+    },
+    portal::{Item, Keyring},
+    Key,
+};
+use zbus::{
+    proxy::ProxyDefault,
+    zvariant::{ObjectPath, OwnedObjectPath, OwnedValue, Value},
+    ObjectServer, SignalContext,
+};
+
+use super::{
+    collection::Collection, error::ServiceError, prompt::Prompt, session::Session, Result,
+};
+
+#[derive(Debug)]
+pub struct Service {
+    collections: Vec<Collection>,
+    keyring: Arc<Keyring>,
+    cnx: Mutex<Option<zbus::Connection>>,
+}
+
+#[zbus::interface(name = "org.freedesktop.Secret.Service")]
+impl Service {
+    pub async fn open_session(
+        &self,
+        algorithm: Algorithm,
+        input: Value<'_>,
+    ) -> Result<(OwnedValue, OwnedObjectPath)> {
+        let client_public_key = match algorithm {
+            Algorithm::Plain => None,
+            Algorithm::Encrypted => Some(Key::from(input)),
+        };
+        let (session, key) = Session::new(client_public_key);
+        // TODO: clean up the default generated key
+        let key = key
+            .map(|k| OwnedValue::from(&k))
+            .unwrap_or_else(|| Value::new::<Vec<u8>>(vec![]).try_to_owned().unwrap());
+        Ok((key, session.path().into()))
+    }
+
+    #[zbus(out_args("collection", "prompt"))]
+    pub async fn create_collection(
+        &self,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        properties: Properties,
+        alias: &str,
+        #[zbus(object_server)] object_server: &ObjectServer,
+    ) -> Result<(OwnedObjectPath, Prompt)> {
+        let collection = Collection::new(properties.label(), alias, Arc::clone(&self.keyring)); // temporarily
+        let path = OwnedObjectPath::from(collection.path());
+        object_server.at(&path, collection).await?;
+        let prompt = Prompt::default(); // temp Prompt
+        Self::collection_created(&ctxt, path.as_ref())
+            .await
+            .map_err::<ServiceError, _>(From::from)?;
+        Ok((path, prompt))
+    }
+
+    #[zbus(out_args("unlocked", "locked"))]
+    pub async fn search_items(
+        &self,
+        attributes: HashMap<&str, &str>,
+    ) -> Result<(Vec<Item>, Vec<Item>)> {
+        let items = self
+            .keyring
+            .search_items(&attributes)
+            .await
+            .map_err::<ServiceError, _>(From::from)?;
+
+        let mut unlocked: Vec<Item> = Vec::new();
+        let mut locked: Vec<Item> = Vec::new();
+
+        for item in items {
+            let attributes = item.attributes();
+            if attributes.get("locked").is_some() {
+                // this if condition is probably wrong
+                // how to access &AttributeValue value
+                locked.push(item)
+            } else {
+                unlocked.push(item)
+            }
+        }
+
+        Ok((unlocked, locked))
+    }
+
+    #[zbus(out_args("unlocked", "prompt"))]
+    pub async fn unlock(
+        &mut self,
+        objects: Vec<OwnedObjectPath>,
+    ) -> Result<(Vec<OwnedObjectPath>, Prompt)> {
+        // manage unlock state in memory
+        // when do we need to prompt?
+        let mut unlocked: Vec<OwnedObjectPath> = Vec::new();
+
+        'main: for object in objects {
+            for collection in self.collections.iter() {
+                if collection.path() == *object {
+                    if collection.locked() {
+                        collection.set_locked(false).await;
+                        unlocked.push(object.clone());
+                    } else {
+                        break 'main;
+                    }
+                }
+            }
+        }
+
+        if unlocked.is_empty() {
+            unlocked.push(OwnedObjectPath::default());
+        }
+
+        let prompt = Prompt::default(); // temporarily
+
+        Ok((unlocked, prompt))
+    }
+
+    #[zbus(out_args("locked", "prompt"))]
+    pub async fn lock(
+        &mut self,
+        objects: Vec<OwnedObjectPath>,
+    ) -> Result<(Vec<OwnedObjectPath>, Prompt)> {
+        // manage lock state in memory
+        // when do we need to prompt?
+        let mut locked: Vec<OwnedObjectPath> = Vec::new();
+
+        for object in objects {
+            for collection in self.collections.iter() {
+                if collection.path() == *object && !collection.locked() {
+                    collection.set_locked(true).await;
+                    locked.push(object.clone());
+                }
+            }
+        }
+
+        if locked.is_empty() {
+            locked.push(OwnedObjectPath::default());
+        }
+
+        let prompt = Prompt::default(); // temporarily
+
+        Ok((locked, prompt))
+    }
+
+    pub async fn get_secrets(
+        &self,
+        paths: Vec<OwnedObjectPath>,
+        session: ObjectPath<'_>,
+    ) -> Result<HashMap<OwnedObjectPath, SecretInner>> {
+        let mut secrets = HashMap::with_capacity(paths.len());
+        for collection in &self.collections {
+            let items = collection.items.read().await;
+            for item in items.iter() {
+                for path in paths.iter() {
+                    if item.path() == path.as_ref() {
+                        let secret = item.secret(session.clone()).await?;
+                        secrets.insert(path.to_owned(), secret);
+                    }
+                }
+            }
+        }
+        Ok(secrets)
+    }
+
+    pub fn read_alias(&self, name: &str) -> ObjectPath {
+        self.collections
+            .iter()
+            .find_map(|c| {
+                if c.label() == name {
+                    Some(c.path())
+                } else {
+                    None
+                }
+            })
+            .unwrap_or_default()
+    }
+
+    pub async fn set_alias(
+        &self,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        alias: &str,
+        path: ObjectPath<'_>,
+    ) -> Result<()> {
+        // WIP: not complete:: handle alias & default path '/' to remove the alias
+        match self.collections.iter().find(|c| c.path() == path) {
+            Some(collection) => {
+                collection.set_alias(&ctxt, alias).await?;
+                Ok(())
+            }
+            None => {
+                tracing::error!("Collection {path} not found");
+                Err(ServiceError::NoSuchObject)
+            }
+        }
+    }
+
+    #[zbus(property, name = "Collections")]
+    pub fn collections(&self) -> Vec<ObjectPath> {
+        self.collections
+            .iter()
+            .map(|collection| collection.path())
+            .collect()
+    }
+
+    #[zbus(signal)]
+    pub async fn collection_created(
+        ctxt: &SignalContext<'_>,
+        collection: ObjectPath<'_>,
+    ) -> zbus::Result<()>;
+
+    #[zbus(signal)]
+    pub async fn collection_deleted(
+        ctxt: &SignalContext<'_>,
+        collection: ObjectPath<'_>,
+    ) -> zbus::Result<()>;
+
+    #[zbus(signal)]
+    pub async fn collection_changed(
+        ctxt: &SignalContext<'_>,
+        collection: ObjectPath<'_>,
+    ) -> zbus::Result<()>;
+}
+
+impl Service {
+    pub async fn new() -> Self {
+        Self {
+            collections: Vec::new(),
+            keyring: Arc::new(Keyring::load_default().await.unwrap()),
+            cnx: Default::default(),
+        }
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let cnx = zbus::connection::Builder::session()?
+            //.name(oo7::dbus::api::Service::DESTINATION.unwrap())?
+            .name("org.freedesktop.secrets.Devel")?
+            .build()
+            .await?;
+        *self.cnx.lock().unwrap() = Some(cnx.clone());
+        cnx.object_server()
+            .at(oo7::dbus::api::Service::PATH.unwrap(), self)
+            .await?;
+        Ok(())
+    }
+}
