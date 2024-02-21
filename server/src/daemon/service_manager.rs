13c97a78431272676a7903ff25b08130fcbd2e07
Dhanuka Warusadura <dhanuka@gnome.org>
2024-02-21 16:13:43 +0530

    Updates ServiceManager
    Removed RwLock wrapper from sessions
    Removed async from all the methods
    
    See: https://github.com/bilelmoussaoui/oo7/pull/73#discussion_r1497239325
    
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

--- server/src/daemon/service_manager.rs
+++ server/src/daemon/service_manager.rs
@@ -1,36 +1,30 @@
 use std::collections::HashMap;
 
-use tokio::sync::RwLock;
 use zbus::zvariant::{ObjectPath, OwnedObjectPath};
 
 use super::session::Session;
 
 #[derive(Debug)]
 pub struct ServiceManager {
-    sessions: RwLock<HashMap<OwnedObjectPath, Session>>,
+    sessions: HashMap<OwnedObjectPath, Session>,
 }
 
 impl ServiceManager {
     pub fn new() -> Self {
         Self {
-            sessions: RwLock::new(HashMap::new()),
+            sessions: HashMap::new(),
         }
     }
 
-    pub async fn session(&self, path: ObjectPath<'_>) -> Option<Session> {
-        self.sessions
-            .read()
-            .await
-            .get(&path.into())
-            .to_owned()
-            .cloned()
+    pub fn session(&self, path: ObjectPath<'_>) -> Option<Session> {
+        self.sessions.get(&path.into()).to_owned().cloned()
     }
 
-    pub async fn insert_session(&mut self, path: ObjectPath<'_>, session: Session) {
-        self.sessions.write().await.insert(path.into(), session);
+    pub fn insert_session(&mut self, path: ObjectPath<'_>, session: Session) {
+        self.sessions.insert(path.into(), session);
     }
 
-    pub async fn remove_session(&mut self, path: ObjectPath<'_>) {
-        self.sessions.write().await.remove(&path.into());
+    pub fn remove_session(&mut self, path: ObjectPath<'_>) {
+        self.sessions.remove(&path.into());
     }
 }
