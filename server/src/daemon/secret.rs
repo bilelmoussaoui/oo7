2e6390684281ee62cba05103937b75c50e0197a1
Dhanuka Warusadura <dhanuka@gnome.org>
2024-03-05 22:02:13 +0530

    Introduce server side Secret API
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

---
+++ server/src/daemon/secret.rs
@@ -0,0 +1,41 @@
+use oo7::{crypto, Key};
+
+use super::session::Session;
+
+pub struct Secret {
+    session: Session,
+    parameters: Vec<u8>,
+    value: Vec<u8>,
+    content_type: String,
+}
+
+impl Secret {
+    pub fn new(session: Session, secret: impl AsRef<[u8]>, content_type: &str) -> Self {
+        Self {
+            session,
+            parameters: vec![],
+            value: secret.as_ref().to_vec(),
+            content_type: content_type.to_owned(),
+        }
+    }
+
+    pub fn new_encrypted(
+        session: Session,
+        secret: impl AsRef<[u8]>,
+        content_type: &str,
+        aes_key: &Key,
+    ) -> Self {
+        let iv = crypto::generate_iv();
+        let secret = crypto::encrypt(secret.as_ref(), aes_key, &iv);
+        Self {
+            session,
+            parameters: iv,
+            value: secret,
+            content_type: content_type.to_owned(),
+        }
+    }
+
+    pub fn value(&self) -> Vec<u8> {
+        self.value.to_owned()
+    }
+}
