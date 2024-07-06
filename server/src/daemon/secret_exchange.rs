63666d2d2bbf8f6f3c69e4c026490d2c8ea8c103
Dhanuka Warusadura <dhanuka@gnome.org>
2024-07-06 22:55:28 +0530

    Cleanup SecretExchange implementation
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

--- server/src/daemon/secret_exchange.rs
+++ server/src/daemon/secret_exchange.rs
@@ -12,11 +12,15 @@ use std::collections::HashMap;
 
 use base64::prelude::*;
 use oo7::{crypto, Key};
+use zeroize::Zeroizing;
 
 const SECRET: &str = "secret";
 const PUBLIC: &str = "public";
+const PRIVATE: &str = "private";
 const IV: &str = "iv";
 const PROTOCOL: &str = "[sx-aes-1]\n";
+const CIPHER_TEXT_LEN: usize = 16;
+const IV_LEN: usize = 16;
 
 #[derive(Debug)]
 pub struct SecretExchange {
@@ -32,50 +36,20 @@ impl SecretExchange {
         encode(&map)
     }
 
-    // Decrypt and retrieve secret
-    pub fn receive(&self, exchange: &str) -> String {
+    // Creates the shared secret: an AES key
+    pub fn create_shared_secret(&self, exchange: &str) -> String {
         let decoded = decode(exchange).unwrap();
-        let mut encrypted: Vec<u8> = Vec::new();
-        let mut map: HashMap<&str, &[u8]> = HashMap::new();
-
-        let secret = decoded.get(SECRET);
-        if secret.is_some() {
-            let secret = secret.unwrap();
-            let public_key = Key::new(decoded.get(PUBLIC).unwrap().to_vec());
-            let iv = decoded.get(IV).unwrap();
-
-            let aes_key = Key::generate_aes_key(&self.private_key, &public_key);
-            encrypted = crypto::decrypt(secret, &aes_key, &iv).to_vec();
-
-            map.insert(SECRET, &encrypted);
-        }
-        map.insert(PUBLIC, self.public_key.as_ref());
-
-        encode(&map)
-    }
-
-    // Send Secret and perform encryption
-    pub fn send(&self, secret: &str, exchange: &str) -> String {
-        let decoded = decode(exchange).unwrap();
-
-        let public_key = Key::new(decoded.get(PUBLIC).unwrap().to_vec());
-        let aes_key = Key::generate_aes_key(&self.private_key, &public_key);
-        let iv = crypto::generate_iv();
-
-        let secret = crypto::encrypt(secret, &aes_key, &iv);
-
-        let map = HashMap::from([
-            (PUBLIC, self.public_key.as_ref()),
-            (SECRET, secret.as_ref()),
-            (IV, iv.as_ref()),
-        ]);
+        let server_public_key = Key::new(decoded.get(PUBLIC).unwrap().to_vec());
+        let aes_key =
+            Key::generate_aes_key_for_secret_exchange(&self.private_key, &server_public_key);
+        let map = HashMap::from([(PRIVATE, aes_key.as_ref())]);
 
         encode(&map)
     }
 
     pub fn new() -> Self {
         let private_key = Key::generate_private_key();
-        let public_key = Key::generate_public_key(&private_key);
+        let public_key = Key::generate_public_key_for_secret_exchange(&private_key);
 
         Self {
             private_key,
@@ -116,14 +90,31 @@ fn decode(exchange: &str) -> Result<HashMap<&str, Vec<u8>>, base64::DecodeError>
     Ok(map)
 }
 
-// Retrieve secret from the payload
-pub fn get_secret(exchange: &str) -> Result<String, std::str::Utf8Error> {
-    let decoded = decode(&exchange).unwrap();
-    let secret = std::str::from_utf8(&decoded.get(SECRET).unwrap().to_vec())?
-        .to_string()
-        .to_owned();
+// Retrieves the secret from final secret exchange string
+pub(crate) fn retrieve_secret(exchange: &str, aes_key: &str) -> Option<Zeroizing<Vec<u8>>> {
+    let decoded = decode(exchange).unwrap();
+    let secret = decoded.get(SECRET);
+    if secret.is_none() {
+        // if we cancel an ongoing prompt call, the final exchange won't have the secret
+        // or iv. this check is implemented to avoid `Option::unwrap()` on a `None`
+        // value
+        return None;
+    }
+    let secret = secret.unwrap();
+    let iv = decoded.get(IV).unwrap();
 
-    Ok(secret)
+    if iv.len() != IV_LEN {
+        panic!("Invalid IV");
+    }
+
+    if secret.len() != CIPHER_TEXT_LEN {
+        panic!("Invalid length for cipher text");
+    }
+
+    let decoded = decode(aes_key).unwrap();
+    let aes_key = Key::new(decoded.get(PRIVATE).unwrap().to_vec());
+
+    Some(crypto::decrypt(secret, &aes_key, iv))
 }
 
 #[cfg(test)]
@@ -131,15 +122,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_secret_exchange() {
-        let secret = "password";
-        let caller = SecretExchange::new();
-        let callee = SecretExchange::new();
-        let exchange = caller.begin();
-        let exchange = caller.receive(&exchange);
-        let exchange = callee.send(secret, &exchange);
-        let exchange = caller.receive(&exchange);
+    fn test_retrieve_secret() {
+        let exchange = "[sx-aes-1]
+public=/V6FpknNXlOGJwPqXtN0RaED2bS5JyYbftv7WbD0gWiVTMoNgxkAuOX2g+zUO/4TdfBJ6viPRcNdYV+KcxskGvhYouFXs+IgKqNO0MF0CNnWra1I6G56SM4Bgstkx9M5J+1f83l/BTAxlLsAppeLkqEEVSQoy9jXhPOrl5XlIzF2DvriYh+FInB7SFz4VzE3KVq40p7tA9+iAVQg1o9qkQHLazFb1DfbWRgvhDVhwNkk1fIlepIeM426gdmHIAxP
+secret=DBeLBvEgGuGygDm+XnkxyQ==
+iv=8e3N+gx553PgQlfTKRK3JA==";
 
-        assert_eq!(get_secret(&exchange).unwrap(), secret);
+        let aes_key = "[sx-aes-1]
+private=zDWLKDent/C//LquHCTlGg==";
+
+        let decrypted = retrieve_secret(exchange, aes_key).unwrap();
+        assert_eq!(b"password".to_vec(), decrypted.to_vec());
     }
 }
