dbcdb662431254427683acf873dd5a75eae9978d
Dhanuka Warusadura <dhanuka@gnome.org>
2024-07-06 15:50:04 +0530

    Use DH with the 1536 ike modp group for secret exchange
    Original author: Daiki Ueno <ueno@gnu.org>
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

--- client/src/crypto/native.rs
+++ client/src/crypto/native.rs
@@ -1,6 +1,6 @@
 use std::{
     ops::{Mul, Rem, Shr},
-    sync::LazyLock,
+    sync::{LazyLock, OnceLock},
 };
 
 use cipher::{
@@ -81,6 +81,17 @@ pub(crate) fn generate_public_key(private_key: impl AsRef<[u8]>) -> Vec<u8> {
     public_key_uint.to_bytes_be()
 }
 
+pub(crate) fn generate_public_key_for_secret_exchange(private_key: impl AsRef<[u8]>) -> Vec<u8> {
+    let private_key_uint = BigUint::from_bytes_be(private_key.as_ref());
+    let dh_generator = {
+        static DH_GENERATOR: OnceLock<BigUint> = OnceLock::new();
+        DH_GENERATOR.get_or_init(|| BigUint::from_u64(0x2).unwrap())
+    };
+    let public_key_uint = powm_for_secret_exchange(dh_generator, private_key_uint);
+
+    public_key_uint.to_bytes_be()
+}
+
 pub(crate) fn generate_aes_key(
     private_key: impl AsRef<[u8]>,
     server_public_key: impl AsRef<[u8]>,
@@ -91,6 +102,35 @@ pub(crate) fn generate_aes_key(
 
     let mut common_secret_bytes = common_secret.to_bytes_be();
     let mut common_secret_padded = vec![0; 128 - common_secret_bytes.len()];
+    // inefficient, but ok for now
+    common_secret_padded.append(&mut common_secret_bytes);
+
+    // hkdf
+    // input_keying_material
+    let ikm = common_secret_padded;
+    let salt = None;
+    let info = [];
+
+    // output keying material
+    let mut okm = Zeroizing::new(vec![0; 16]);
+
+    let (_, hk) = Hkdf::<Sha256>::extract(salt, &ikm);
+    hk.expand(&info, okm.as_mut())
+        .expect("hkdf expand should never fail");
+
+    okm
+}
+
+pub(crate) fn generate_aes_key_for_secret_exchange(
+    private_key: impl AsRef<[u8]>,
+    server_public_key: impl AsRef<[u8]>,
+) -> Zeroizing<Vec<u8>> {
+    let server_public_key_uint = BigUint::from_bytes_be(server_public_key.as_ref());
+    let private_key_uint = BigUint::from_bytes_be(private_key.as_ref());
+    let common_secret = powm_for_secret_exchange(&server_public_key_uint, private_key_uint);
+
+    let mut common_secret_bytes = common_secret.to_bytes_be();
+    let mut common_secret_padded = vec![0; 192 - common_secret_bytes.len()];
     // inefficient, but ok for now
     common_secret_padded.append(&mut common_secret_bytes);
 
@@ -228,6 +268,45 @@ fn powm(base: &BigUint, mut exp: BigUint) -> BigUint {
         }
         exp = exp.shr(1);
         base = (&base).mul(&base).rem(&*DH_PRIME);
+    }
+    exp.zeroize();
+
+    result
+}
+
+fn powm_for_secret_exchange(base: &BigUint, mut exp: BigUint) -> BigUint {
+    let modulus = {
+        // for key exchange
+        static DH_PRIME: OnceLock<BigUint> = OnceLock::new();
+        DH_PRIME.get_or_init(|| {
+            BigUint::from_bytes_be(&[
+                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xC9, 0x0F, 0xDA, 0xA2, 0x21, 0x68,
+                0xC2, 0x34, 0xC4, 0xC6, 0x62, 0x8B, 0x80, 0xDC, 0x1C, 0xD1, 0x29, 0x02, 0x4E, 0x08,
+                0x8A, 0x67, 0xCC, 0x74, 0x02, 0x0B, 0xBE, 0xA6, 0x3B, 0x13, 0x9B, 0x22, 0x51, 0x4A,
+                0x08, 0x79, 0x8E, 0x34, 0x04, 0xDD, 0xEF, 0x95, 0x19, 0xB3, 0xCD, 0x3A, 0x43, 0x1B,
+                0x30, 0x2B, 0x0A, 0x6D, 0xF2, 0x5F, 0x14, 0x37, 0x4F, 0xE1, 0x35, 0x6D, 0x6D, 0x51,
+                0xC2, 0x45, 0xE4, 0x85, 0xB5, 0x76, 0x62, 0x5E, 0x7E, 0xC6, 0xF4, 0x4C, 0x42, 0xE9,
+                0xA6, 0x37, 0xED, 0x6B, 0x0B, 0xFF, 0x5C, 0xB6, 0xF4, 0x06, 0xB7, 0xED, 0xEE, 0x38,
+                0x6B, 0xFB, 0x5A, 0x89, 0x9F, 0xA5, 0xAE, 0x9F, 0x24, 0x11, 0x7C, 0x4B, 0x1F, 0xE6,
+                0x49, 0x28, 0x66, 0x51, 0xEC, 0xE4, 0x5B, 0x3D, 0xC2, 0x00, 0x7C, 0xB8, 0xA1, 0x63,
+                0xBF, 0x05, 0x98, 0xDA, 0x48, 0x36, 0x1C, 0x55, 0xD3, 0x9A, 0x69, 0x16, 0x3F, 0xA8,
+                0xFD, 0x24, 0xCF, 0x5F, 0x83, 0x65, 0x5D, 0x23, 0xDC, 0xA3, 0xAD, 0x96, 0x1C, 0x62,
+                0xF3, 0x56, 0x20, 0x85, 0x52, 0xBB, 0x9E, 0xD5, 0x29, 0x07, 0x70, 0x96, 0x96, 0x6D,
+                0x67, 0x0C, 0x35, 0x4E, 0x4A, 0xBC, 0x98, 0x04, 0xF1, 0x74, 0x6C, 0x08, 0xCA, 0x23,
+                0x73, 0x27, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            ])
+        })
+    };
+
+    let mut base = base.clone();
+    let mut result: BigUint = One::one();
+
+    while !exp.is_zero() {
+        if exp.is_odd() {
+            result = result.mul(&base).rem(modulus);
+        }
+        exp = exp.shr(1);
+        base = (&base).mul(&base).rem(modulus);
     }
     exp.zeroize();
 
