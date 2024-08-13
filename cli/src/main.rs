d2bd0b19b4f2dbf238b6750c6011699cf589c892
Bilal Elmoussaoui <belmouss@redhat.com>
2024-08-13 13:13:34 +0200

    cli: Write secret directly to stdout

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fmt,
-    io::Write,
+    io::{IsTerminal, Write},
     process::{ExitCode, Termination},
 };
 
@@ -17,6 +17,12 @@ struct Error(String);
 impl fmt::Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error(err.to_string())
     }
 }
 
@@ -58,6 +64,8 @@ enum Commands {
     Lookup {
         #[arg(help = "List of attributes. This is a space separated list of pairs of key value", value_parser = parse_key_val::<String, String>)]
         attributes: Vec<(String, String)>,
+        #[arg(long, help = "Print only the secret.")]
+        secret_only: bool,
     },
 
     #[command(
@@ -74,6 +82,8 @@ enum Commands {
         all: bool,
         #[arg(help = "List of attributes. This is a space separated list of pairs of key value", value_parser = parse_key_val::<String, String>)]
         attributes: Vec<(String, String)>,
+        #[arg(long, help = "Print only the secret.")]
+        secret_only: bool,
     },
 
     #[command(
@@ -107,8 +117,15 @@ async fn main() -> Result<(), Error> {
     let cli = Cli::parse();
     match cli.command {
         Commands::Store { label, attributes } => store(&label, &attributes).await,
-        Commands::Lookup { attributes } => lookup(&attributes).await,
-        Commands::Search { attributes, all } => search(&attributes, all).await,
+        Commands::Lookup {
+            attributes,
+            secret_only,
+        } => lookup(&attributes, secret_only).await,
+        Commands::Search {
+            attributes,
+            all,
+            secret_only,
+        } => search(&attributes, all, secret_only).await,
         Commands::Delete { attributes } => delete(&attributes).await,
         Commands::Lock => lock().await,
         Commands::Unlock => unlock().await,
@@ -147,27 +164,27 @@ async fn store(label: &str, attributes: &impl AsAttributes) -> Result<(), Error>
     Ok(())
 }
 
-async fn lookup(attributes: &impl AsAttributes) -> Result<(), Error> {
+async fn lookup(attributes: &impl AsAttributes, secret_only: bool) -> Result<(), Error> {
     let collection = collection().await?;
     let items = collection.search_items(attributes).await?;
 
     if let Some(item) = items.first() {
-        print_item(item).await?;
+        print_item(item, secret_only).await?;
     }
 
     Ok(())
 }
 
-async fn search(attributes: &impl AsAttributes, all: bool) -> Result<(), Error> {
+async fn search(attributes: &impl AsAttributes, all: bool, secret_only: bool) -> Result<(), Error> {
     let collection = collection().await?;
     let items = collection.search_items(attributes).await?;
 
     if all {
         for item in items {
-            print_item(&item).await?;
+            print_item(&item, secret_only).await?;
         }
     } else if let Some(item) = items.first() {
-        print_item(item).await?;
+        print_item(item, secret_only).await?;
     }
 
     Ok(())
@@ -198,50 +215,59 @@ async fn unlock() -> Result<(), Error> {
     Ok(())
 }
 
-async fn print_item<'a>(item: &oo7::dbus::Item<'a>) -> Result<(), Error> {
+async fn print_item<'a>(item: &oo7::dbus::Item<'a>, secret_only: bool) -> Result<(), Error> {
     use std::fmt::Write;
-
-    let label = item.label().await?;
-    let bytes = item.secret().await?;
-    let mut attributes = item.attributes().await?;
-    let created = item.created().await?;
-    let modified = item.modified().await?;
-
-    let created = chrono::DateTime::<chrono::Utc>::from_timestamp(created.as_secs() as i64, 0)
-        .unwrap()
-        .with_timezone(&chrono::Local);
-    let modified = chrono::DateTime::<chrono::Utc>::from_timestamp(modified.as_secs() as i64, 0)
-        .unwrap()
-        .with_timezone(&chrono::Local);
-
-    let mut result = format!("[{label}]\n");
-    match std::str::from_utf8(&bytes) {
-        Ok(secret) => {
-            writeln!(&mut result, "secret = {secret}").unwrap();
+    if secret_only {
+        let bytes = item.secret().await?;
+        let mut stdout = std::io::stdout().lock();
+        stdout.write_all(&bytes)?;
+        // Add a new line if we are writing to a tty
+        if stdout.is_terminal() {
+            stdout.write_all(b"\n")?;
         }
-        Err(_) => {
-            writeln!(&mut result, "secret = {:02X?}", bytes.as_slice()).unwrap();
+    } else {
+        let label = item.label().await?;
+        let bytes = item.secret().await?;
+        let mut attributes = item.attributes().await?;
+        let created = item.created().await?;
+        let modified = item.modified().await?;
+
+        let created = chrono::DateTime::<chrono::Utc>::from_timestamp(created.as_secs() as i64, 0)
+            .unwrap()
+            .with_timezone(&chrono::Local);
+        let modified =
+            chrono::DateTime::<chrono::Utc>::from_timestamp(modified.as_secs() as i64, 0)
+                .unwrap()
+                .with_timezone(&chrono::Local);
+
+        let mut result = format!("[{label}]\n");
+        match std::str::from_utf8(&bytes) {
+            Ok(secret) => {
+                writeln!(&mut result, "secret = {secret}").unwrap();
+            }
+            Err(_) => {
+                writeln!(&mut result, "secret = {:02X?}", bytes.as_slice()).unwrap();
+            }
         }
-    }
 
-    writeln!(
-        &mut result,
-        "created = {}",
-        created.format("%Y-%m-%d %H:%M:%S")
-    )
-    .unwrap();
-    writeln!(
-        &mut result,
-        "modified = {}",
-        modified.format("%Y-%m-%d %H:%M:%S")
-    )
-    .unwrap();
-    if let Some(schema) = attributes.remove("xdg:schema") {
-        writeln!(&mut result, "schema = {schema} ").unwrap();
+        writeln!(
+            &mut result,
+            "created = {}",
+            created.format("%Y-%m-%d %H:%M:%S")
+        )
+        .unwrap();
+        writeln!(
+            &mut result,
+            "modified = {}",
+            modified.format("%Y-%m-%d %H:%M:%S")
+        )
+        .unwrap();
+        if let Some(schema) = attributes.remove("xdg:schema") {
+            writeln!(&mut result, "schema = {schema} ").unwrap();
+        }
+        writeln!(&mut result, "attributes = {attributes:?} ").unwrap();
+        print!("{result}");
     }
-    writeln!(&mut result, "attributes = {attributes:?} ").unwrap();
-    print!("{result}");
-
     Ok(())
 }
 
