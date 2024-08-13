31d9c87e2a861a6f95ace26c8eb4f63f97f4a310
Bilal Elmoussaoui <belmouss@redhat.com>
2024-08-13 12:47:51 +0200

    portal: Switch to using ashpd

--- portal/src/main.rs
+++ portal/src/main.rs
@@ -1,84 +1,42 @@
-mod error;
-mod request;
-
 use std::{collections::HashMap, future::pending, os::unix::net::UnixStream};
+mod error;
 
+use ashpd::{
+    async_trait,
+    zbus::{self, zvariant::OwnedValue, ProxyDefault},
+    AppID,
+};
 use clap::Parser;
-use futures_util::FutureExt;
+pub use error::Result;
 use oo7::dbus::Service;
 use tokio::io::AsyncWriteExt;
-use zbus::{
-    zvariant::{self, OwnedObjectPath},
-    ProxyDefault,
-};
 
-use crate::{
-    error::Error,
-    request::{Request, ResponseType},
-};
-
-const PORTAL_VERSION: u32 = 1;
 const PORTAL_SECRET_SIZE: usize = 64;
 const PORTAL_NAME: &str = "org.freedesktop.impl.portal.desktop.oo7";
 
 struct Secret;
 
-#[zbus::interface(name = "org.freedesktop.impl.portal.Secret")]
-impl Secret {
-    #[dbus_interface(property, name = "version")]
-    fn version(&self) -> u32 {
-        PORTAL_VERSION
-    }
+#[async_trait::async_trait]
+impl ashpd::backend::request::RequestImpl for Secret {
+    async fn close(&self) {}
+}
 
-    #[dbus_interface(out_args("response", "results"))]
-    async fn retrieve_secret(
+#[async_trait::async_trait]
+impl ashpd::backend::secret::SecretImpl for Secret {
+    async fn retrieve(
         &self,
-        #[zbus(object_server)] object_server: &zbus::ObjectServer,
-        handle: OwnedObjectPath,
-        app_id: &str,
-        fd: zvariant::OwnedFd,
-        options: HashMap<&str, zvariant::Value<'_>>,
-    ) -> Result<(ResponseType, HashMap<&str, zvariant::OwnedValue>), Error> {
-        tracing::debug!("Request from app: {app_id} with options: {options:?}");
-
-        let (sender, receiver) = futures_channel::oneshot::channel();
-        let request = Request::new(&handle, sender);
-        object_server.at(&handle, request).await?;
-
-        let fut_1 = async move {
-            let res = match send_secret_to_app(app_id, fd).await {
-                Ok(_) => ResponseType::Success,
-                Err(err) => {
-                    tracing::error!("could not retrieve secret: {err}");
-                    ResponseType::Other
-                }
-            };
-
-            // We do not accept Close request anymore here.
-            tracing::debug!("Request {handle} handled");
-            object_server.remove::<Request, _>(&handle).await.unwrap();
-
-            Ok((res, HashMap::new()))
-        };
-
-        let fut_2 = async move {
-            receiver.await.unwrap();
-            Ok((ResponseType::Cancelled, HashMap::new()))
-        };
-
-        let t1 = fut_1.fuse();
-        let t2 = fut_2.fuse();
-
-        futures_util::pin_mut!(t1, t2);
-
-        futures_util::select! {
-            fut_1_res = t1 => fut_1_res,
-            fut_2_res = t2 => fut_2_res,
-        }
+        app_id: ashpd::AppID,
+        fd: std::os::fd::OwnedFd,
+    ) -> ashpd::backend::Result<HashMap<String, OwnedValue>> {
+        tracing::debug!("Request from app: {app_id}");
+        send_secret_to_app(&app_id, fd)
+            .await
+            .map_err(|e| ashpd::PortalError::Failed(format!("Could not retrieve secret {e}")))?;
+        Ok(Default::default())
     }
 }
 
-fn generate_secret() -> Result<zeroize::Zeroizing<Vec<u8>>, Error> {
+fn generate_secret() -> Result<zeroize::Zeroizing<Vec<u8>>> {
     let mut secret = [0; PORTAL_SECRET_SIZE];
     // Equivalent of `ring::rand::SecureRandom`
     getrandom::getrandom(&mut secret)?;
@@ -86,7 +44,7 @@ fn generate_secret() -> Result<zeroize::Zeroizing<Vec<u8>>, Error> {
 }
 
 /// Generates, stores and send the secret back to the fd stream
-async fn send_secret_to_app(app_id: &str, fd: zvariant::OwnedFd) -> Result<(), Error> {
+async fn send_secret_to_app(app_id: &AppID, fd: std::os::fd::OwnedFd) -> Result<()> {
     let service = Service::new().await?;
     let collection = match service.default_collection().await {
         Err(oo7::dbus::Error::NotFound(_)) => {
@@ -103,6 +61,7 @@ async fn send_secret_to_app(app_id: &str, fd: zvariant::OwnedFd) -> Result<(), E
     } else {
         tracing::debug!("Could not find secret for {app_id}, creating one");
         let secret = generate_secret()?;
+
         collection
             .create_item(
                 &format!("Secret Portal token for {app_id}"),
@@ -118,8 +77,7 @@ async fn send_secret_to_app(app_id: &str, fd: zvariant::OwnedFd) -> Result<(), E
     };
 
     // Write the secret to the FD.
-    let mut stream =
-        tokio::net::UnixStream::from_std(UnixStream::from(std::os::fd::OwnedFd::from(fd)))?;
+    let mut stream = tokio::net::UnixStream::from_std(UnixStream::from(fd))?;
     stream.write_all(&secret).await?;
 
     Ok(())
@@ -135,7 +93,7 @@ struct Args {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), zbus::Error> {
+async fn main() -> Result<()> {
     let args = Args::parse();
 
     tracing_subscriber::fmt::init();
@@ -146,10 +104,12 @@ async fn main() -> Result<(), zbus::Error> {
         env!("CARGO_PKG_VERSION")
     );
 
-    let backend = Secret;
-    let cnx = zbus::ConnectionBuilder::session()?
-        .serve_at(oo7::portal::SecretProxy::PATH.unwrap(), backend)?
-        .build()
+    let cnx = zbus::Connection::session().await?;
+
+    let portal = ashpd::backend::secret::SecretInterface::new(Secret, cnx.clone());
+    tracing::debug!("Serving `org.freedesktop.impl.portal.Secret`");
+    cnx.object_server()
+        .at(oo7::portal::SecretProxy::PATH.unwrap(), portal)
         .await?;
 
     let mut flags = zbus::fdo::RequestNameFlags::AllowReplacement.into();
