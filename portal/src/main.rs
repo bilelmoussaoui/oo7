9fd202d596bf99046cc013be8f43b4ade010f59f
Maximiliano Sandoval <msandova@protonmail.com>
2024-09-22 11:10:19 +0200

    portal: Set generic xdg:schema on secret
    This is done by gnome-keyring.

--- portal/src/main.rs
+++ portal/src/main.rs
@@ -47,8 +47,15 @@ fn generate_secret() -> Result<zeroize::Zeroizing<Vec<u8>>> {
 async fn send_secret_to_app(app_id: &AppID, fd: std::os::fd::OwnedFd) -> Result<()> {
     let service = Service::new().await?;
     let collection = service.default_collection().await?;
-    let attributes = HashMap::from([("app_id", app_id)]);
+    // Generic schema, used by gnome-keyring-daemon and used for backward
+    // compatibility purposes TODO: figure out if kwallet or other portal
+    // implementations make use of it, or if it is even useful
+    const GENERIC_SCHEMA_VALUE: &str = "org.freedesktop.Secret.Generic";
 
+    let attributes = HashMap::from([
+        (oo7::XDG_SCHEMA_ATTRIBUTE, GENERIC_SCHEMA_VALUE),
+        ("app_id", app_id),
+    ]);
     let secret = if let Some(item) = collection.search_items(&attributes).await?.first() {
         item.secret().await?
     } else {
