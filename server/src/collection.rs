acfd96c6be9296d24cd87398200e8cc6f07f87ce
Dhanuka Warusadura <dhanuka@gnome.org>
2024-10-20 15:32:46 +0530

    server: Implement Collection::new()
    Also, add all the properties and attributes for the Collection struct.
    
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

--- server/src/collection.rs
+++ server/src/collection.rs
@@ -1,15 +1,34 @@
 // org.freedesktop.Secret.Collection
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{atomic::AtomicBool, Arc},
+    time::{Duration, SystemTime},
+};
 
 use oo7::dbus::api::{Properties, SecretInner};
+use tokio::sync::{Mutex, RwLock};
 use zbus::{interface, zvariant};
 use zvariant::{ObjectPath, OwnedObjectPath};
 
 use super::Result;
+use crate::{item, service_manager::ServiceManager};
 
 #[derive(Debug)]
-pub struct Collection {}
+#[allow(unused)]
+pub struct Collection {
+    // Properties
+    items: Mutex<Vec<item::Item>>,
+    label: Mutex<String>,
+    locked: AtomicBool,
+    created: Duration,
+    modified: Mutex<Duration>,
+    // Other attributes
+    alias: Mutex<String>,
+    manager: Arc<Mutex<ServiceManager>>,
+    n_items: RwLock<i32>,
+    path: OwnedObjectPath,
+}
 
 #[interface(name = "org.freedesktop.Secret.Collection")]
 impl Collection {
@@ -31,5 +50,33 @@ impl Collection {
         _replace: bool,
     ) -> Result<(OwnedObjectPath, ObjectPath)> {
         todo!()
+    }
+}
+
+impl Collection {
+    pub fn new(label: &str, alias: &str, manager: Arc<Mutex<ServiceManager>>) -> Self {
+        let created = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+
+        Self {
+            items: Default::default(),
+            label: Mutex::new(label.to_owned()),
+            locked: AtomicBool::new(true),
+            modified: Mutex::new(created),
+            alias: Mutex::new(alias.to_owned()),
+            n_items: RwLock::new(0),
+            path: OwnedObjectPath::try_from(format!(
+                "/org/freedesktop/secrets/collection/{}",
+                label
+            ))
+            .unwrap(),
+            created,
+            manager,
+        }
+    }
+
+    pub fn path(&self) -> &OwnedObjectPath {
+        &self.path
     }
 }
