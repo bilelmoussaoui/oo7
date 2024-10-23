c49b61570fbae7091352cd6ed415687dad4f4b71
Dhanuka Warusadura <dhanuka@gnome.org>
2024-10-23 08:56:03 +0530

    client: Add Secret::random()
    That generates and returns a random Secret.
    
    Signed-off-by: Dhanuka Warusadura <dhanuka@gnome.org>

--- client/src/portal/secret.rs
+++ client/src/portal/secret.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use zeroize::{Zeroize, ZeroizeOnDrop};
 
 /// Secret used to unlock the keyring.
@@ -15,5 +16,11 @@ impl std::ops::Deref for Secret {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl Secret {
+    pub fn random() -> Self {
+        Self(rand::thread_rng().gen::<[u8; 8]>().to_vec())
     }
 }
