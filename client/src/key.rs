2faf72b7a1d344969abb97584991741bc6d81ce3
Daiki Ueno <dueno@redhat.com>
2024-12-06 14:21:38 +0530

    client: Use DH with the 1536 ike modp group for secret exchange

--- client/src/key.rs
+++ client/src/key.rs
@@ -47,8 +47,21 @@ impl Key {
         Self::new(crypto::generate_public_key(private_key))
     }
 
+    pub fn generate_public_key_for_secret_exchange(private_key: &Self) -> Self {
+        Self::new(crypto::generate_public_key_for_secret_exchange(private_key))
+    }
+
     pub fn generate_aes_key(private_key: &Self, server_public_key: &Self) -> Self {
         Self::new(crypto::generate_aes_key(private_key, server_public_key).to_vec())
+    }
+
+    pub fn generate_aes_key_for_secret_exchange(
+        private_key: &Self,
+        server_public_key: &Self,
+    ) -> Self {
+        Self::new(
+            crypto::generate_aes_key_for_secret_exchange(private_key, server_public_key).to_vec(),
+        )
     }
 }
 
