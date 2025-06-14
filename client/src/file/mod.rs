61d731f63a189b8d08c634b36085705f5fcf7482
Maximiliano Sandoval <msandova@protonmail.com>
2025-06-14 22:08:39 +0200

    file: Add helpers to {un,}locked keyrings

--- client/src/file/mod.rs
+++ client/src/file/mod.rs
@@ -367,6 +367,14 @@ impl Keyring {
 }
 
 impl UnlockedKeyring {
+    pub fn lock(self) -> LockedKeyring {
+        LockedKeyring {
+            mtime: self.mtime,
+            keyring: self.keyring,
+            path: self.path,
+        }
+    }
+
     async fn lookup_item(&self, attributes: &impl AsAttributes) -> Result<Option<Item>, Error> {
         let key = self.derive_key().await?;
         let keyring = self.keyring.read().await;
@@ -689,6 +697,98 @@ impl UnlockedKeyring {
             }
             Err(err) => Err(err),
         }
+    }
+}
+
+impl LockedKeyring {
+    /// Unlocks a keyring and validates it
+    pub async fn unlock(self, secret: Secret) -> Result<UnlockedKeyring, Error> {
+        self.unlock_inner(secret, true).await
+    }
+
+    /// Unlocks a keyring without validating it
+    pub async unsafe fn unlock_unchecked(self, secret: Secret) -> Result<UnlockedKeyring, Error> {
+        self.unlock_inner(secret, false).await
+    }
+
+    async fn unlock_inner(
+        self,
+        secret: Secret,
+        validate_items: bool,
+    ) -> Result<UnlockedKeyring, Error> {
+        let key = if validate_items {
+            let inner_keyring = self.keyring.read().await;
+
+            let key = inner_keyring.derive_key(&secret)?;
+
+            let mut n_broken_items = 0;
+            let mut n_valid_items = 0;
+            for encrypted_item in &inner_keyring.items {
+                if let Err(_err) = encrypted_item.clone().decrypt(&key) {
+                    #[cfg(feature = "tracing")]
+                    tracing::warn!("Failed to decrypt item, incorrect secret?: {_err}");
+                    n_broken_items += 1;
+                } else {
+                    n_valid_items += 1;
+                }
+            }
+
+            drop(inner_keyring);
+
+            if n_broken_items > n_valid_items {
+                #[cfg(feature = "tracing")]
+                {
+                    tracing::warn!(
+                        "The file contains {n_broken_items} broken items and {n_valid_items} valid ones."
+                    );
+                    tracing::info!(
+                        "Please switch to Keyring::load_unchecked to load the keyring without the secret validation"
+                    );
+                    tracing::info!("Keyring::delete_broken_items can be used to remove them");
+                }
+                return Err(Error::IncorrectSecret);
+            }
+            Some(Arc::new(key))
+        } else {
+            None
+        };
+
+        Ok(UnlockedKeyring {
+            keyring: self.keyring,
+            path: self.path,
+            mtime: self.mtime,
+            key: Mutex::new(key),
+            secret: Mutex::new(Arc::new(secret)),
+        })
+    }
+
+    pub async fn load(path: PathBuf) -> Result<Self, Error> {
+        let (mtime, keyring) = match fs::File::open(&path).await {
+            Err(err) if err.kind() == io::ErrorKind::NotFound => {
+                #[cfg(feature = "tracing")]
+                tracing::debug!("Keyring file not found, creating a new one");
+                (None, api::Keyring::new())
+            }
+            Err(err) => return Err(err.into()),
+            Ok(mut file) => {
+                #[cfg(feature = "tracing")]
+                tracing::debug!("Keyring file found, loading it content");
+                let mtime = file.metadata().await?.modified().ok();
+
+                let mut content = Vec::new();
+                file.read_to_end(&mut content).await?;
+
+                let keyring = api::Keyring::try_from(content.as_slice())?;
+
+                (mtime, keyring)
+            }
+        };
+
+        Ok(Self {
+            keyring: Arc::new(RwLock::new(keyring)),
+            path: Some(path),
+            mtime: Mutex::new(mtime),
+        })
     }
 }
 
